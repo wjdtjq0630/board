565fa0cc6dd20016265ef095509a396954f0c862
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-12 23:57:25 +0900

    파일 인코딩 설정

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
   //호스팅 전용 config파일
   $db_host = "localhost";
-  $db_user = "root";
-  $db_password = "qqwwee11";
-  $db_name = "board";
+  $db_user = "damn";
+  $db_password = "wjdtjq0630!";
+  $db_name = "damn";
 
   $conn = mysqli_connect($db_host,$db_user,$db_password,$db_name);
 
@@ -11,7 +11,9 @@
     echo "데이터베이스 연결 실패:".mysqli_connect_error();
   }
 
-  header("Pragma: no-cache");
-  header("Cache-Control: no-cache,must-revalidate");
+  mysqli_query($conn, "set session character_set_connection=utf8;");
+  mysqli_query($conn, "set session character_set_results=utf8;");
+  mysqli_query($conn, "set session character_set_client=utf8;");
+  header("Content-Type: text/html; charset=UTF-8");
 
   ?>
