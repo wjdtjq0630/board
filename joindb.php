e14d8f267b08791ee265bbebd7879ef5daac829e
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-03-13 00:30:29 +0900

    줄바꿈

--- joindb.php
+++ joindb.php
@@ -13,9 +13,10 @@
 		echo '<script>alert("이미 존재하는 아이디 입니다!"); history.back();</script>';
 	} else{
  		 $sql= "INSERT INTO data_board (user_id, user_pw, user_name, user_phn) VALUES ('$id', '$pw', '$name', '$phn')";
- 		 if($result = mysqli_query($conn, $sql)){
-		 mysqli_close($conn);
-		 echo '<script>alert("가입 완료!"); location.href="./list.php"; </script>';
+
+		 if($result = mysqli_query($conn, $sql)){
+		 	mysqli_close($conn);
+		 	echo '<script>alert("가입 완료!"); location.href="./list.php"; </script>';
 		 } else{
 			 	mysqli_close();
 				echo '<script>alert(가입 오류); location.href="./list.php"; </script>';
