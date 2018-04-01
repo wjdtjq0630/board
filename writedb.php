269c41700b52f7321ffa92e1ec762d11a48a7313
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-04-01 23:30:10 +0900

    코드 위치 수정

--- writedb.php
+++ writedb.php
@@ -1,20 +1,21 @@
-	<?php 
+	<?php
  		include 'config.php';
- 		
+
  		$date = $_POST['date'];
  		$title = mysqli_real_escape_string($conn, $_POST['title']);
  		$content = mysqli_real_escape_string($conn, $_POST['content']);
-        $writer = $_POST['writer'];
- 		$sql = "INSERT INTO writing_board (title, content, writer, date) VALUES ('$title', '$content', '$writer', '$date')";
-    	mysqli_query($conn, $sql);
-    	
-    	$sql = "SELECT MAX(id) from writing_board";
-    	$id = mysqli_query($conn, $sql);
+    $writer = $_POST['writer'];
 
-    	$row = mysqli_fetch_array($id);
-    	$id = $row['MAX(id)'];
-    	mysqli_close($conn);
+		$sql = "INSERT INTO writing_board (title, content, writer, date) VALUES ('$title', '$content', '$writer', '$date')";
+    mysqli_query($conn, $sql);
 
-    	$new_URL = "./content.php?id=$id";
-    	header('Location: '.$new_URL);
+    $sql = "SELECT MAX(id) from writing_board";
+    $id = mysqli_query($conn, $sql);
+
+    $row = mysqli_fetch_array($id);
+  	$id = $row['MAX(id)'];
+    mysqli_close($conn);
+
+    $new_URL = "./content.php?id=$id";
+    header('Location: '.$new_URL);
  	 ?>
